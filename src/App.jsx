491d22a06e996b06b9fd1b7d7663ad0b85f3cbf0
Saurabhjaiswar05 <sjsaurabh082@gmail.com>
2024-10-20 00:25:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [data, setData] = useState('');
+  const [come, setCome] = useState([]);
+
+  const changeHandler = (e) => {
+    setData(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setCome([data, ...come]);
+    setData('');
+  };
+
+  const deleteHandler = (indexToRemove) => {
+    const updatedTasks = [...come]; 
+    updatedTasks.splice(indexToRemove, 1); 
+    setCome(updatedTasks); 
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-gray-400">
+        <div className="border overflow-y-auto bg-gray-500 outline-none border-gray-500 w-[450px] rounded-xl h-[500px] p-10">
+          <h1 className="text-center text-white font-bold text-lg">To Do List</h1>
+
+          <form className="p-5 ml-8" onSubmit={submitHandler}>
+            <input
+              type="text"
+              placeholder="Enter your task"
+              value={data}
+              onChange={changeHandler}
+              required
+              className='outline-none rounded p-2'
+            />
+            <button className="bg-black rounded-full text-white py-2 px-4 ml-4">Add</button>
+          </form>
+
+          <div className="p-5  ">
+            {come.length > 0 ? (
+              come.map((task, index) => (
+                <div key={index} className='flex justify-around my-3'>
+                  <h1 className="text-white text-lg">{task}</h1>
+                  <button
+                    className="bg-black text-white py-1 px-3 rounded-full"
+                    onClick={() => deleteHandler(index)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))
+            ) : (
+              <p className="text-white">No tasks yet</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
